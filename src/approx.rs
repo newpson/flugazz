2ca708503c1bbf19221a6de7a68610ddb2cec487
newpson <dk.newpson@gmail.com>
2026-04-21 21:14:34 +0300

    remove unnecessary dependencies; add `Vector2` variants

--- src/approx.rs
+++ src/approx.rs
@@ -1,19 +1,36 @@
-use float_cmp::approx_eq;
+use nalgebra::Vector2;
 
-fn ceil(value: f64) -> f64 {
+pub fn eq_eps(a: f64, b: f64, eps: f64) -> bool {
+    (b - a).abs() < eps
+}
+
+pub fn eq(a: f64, b: f64) -> bool {
+    const EPS: f64 = 1e-6;
+    eq_eps(a, b, EPS)
+}
+
+pub fn ceil(value: f64) -> f64 {
     let floored = f64::floor(value);
     let ceiled = f64::ceil(value);
-    if approx_eq!(f64, value, floored, ulps = 5) {
+    if eq(value, floored) {
         return floored;
     }
     ceiled
 }
 
-fn floor(value: f64) -> f64 {
+pub fn floor(value: f64) -> f64 {
     let floored = f64::floor(value);
     let ceiled = f64::ceil(value);
-    if approx_eq!(f64, value, ceiled, ulps = 5) {
+    if eq(value, ceiled) {
         return ceiled;
     }
     floored
+}
+
+pub fn ceil_vec(vec: Vector2<f64>) -> Vector2<f64> {
+    Vector2::new(ceil(vec.x), ceil(vec.y))
+}
+
+pub fn floor_vec(vec: Vector2<f64>) -> Vector2<f64> {
+    Vector2::new(floor(vec.x), floor(vec.y))
 }
